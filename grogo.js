675c0fcde4b1e123c1f317fcee74a4dc3a9fd931
ajay2603 <124191132+ajay2603@users.noreply.github.com>
2023-04-21 14:57:22 +0530

    Add files via upload

---
+++ grogo.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/gobot.html");
+});
+
+var at1;
+var on1;
+var at2 = 17.122318;
+var on2 = 81.292313;
+app.post('/',function(req,res){
+  at1 = req.body.lat;
+  on1 = req.body.lon;
+  var dic = getDistanceFromLatLonInKm(at1,on1,at2,on2);
+  dic = dic*1000;
+ if(dic>1000){
+  dic=dic/1000;
+  dic= dic.toFixed(2);
+  dic = dic+"km";
+ }
+ else{
+  dic= dic.toFixed(2);
+  dic = dic+"m";
+ }
+ res.send(dic);
+
+})
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    const earthRadiusKm = 6371; // radius of the earth in kilometers
+    const dLat = deg2rad(lat2-lat1);
+    const dLon = deg2rad(lon2-lon1);
+    const a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    const distance = earthRadiusKm * c; // distance in kilometers
+    return distance;
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI/180);
+  }
+  
+ 
+
+ app.listen(process.env.PORT || 3000,function(){
+  console.log("Server running in port 3000 ");
+ })
